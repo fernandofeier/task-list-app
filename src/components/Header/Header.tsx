92cb1748b181f9b021460bc4e48441d6e92f0607
fernandofeier <fern.feier@gmail.com>
2025-04-16 20:18:39 -0300

    Create Header component with title

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="w-full bg-white shadow p-4 flex justify-center">
+      <h1 className="text-2xl font-bold text-gray-800">Minhas Tarefas</h1>
+    </header>
+  );
+};
+
+export default Header;
